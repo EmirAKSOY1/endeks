9af4cbc9eb9d944ecd271d172143d2f7d9d13bdb
EmirAKSOY1 <72320002+EmirAKSOY1@users.noreply.github.com>
2020-10-03 23:24:53 +0300

    Add files via upload

---
+++ vüvutkütleendeksi.py
@@ -0,0 +1,8 @@
+isim=str(input("LÜTFEN İSMİNİZİ GİRİNİZ:"))
+kilo=int(input("LÜTFEN KİLONUZU GİRİNİZ:"))
+boy=float(input("LÜTFEN BOYUNUZU GİRİNİZ:"))
+formul=(kilo/boy**2)
+if(formul<=14.5):
+    print("zayıfsın helal",formul)
+
+
