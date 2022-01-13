8c951e4709ee125baeaffe433fee08795e821c18
EmirAKSOY1 <72320002+EmirAKSOY1@users.noreply.github.com>
2022-01-13 15:08:56 +0300

    Update vüvutkütleendeksi.py

--- vüvutkütleendeksi.py
+++ vüvutkütleendeksi.py
@@ -3,7 +3,6 @@ kilo=int(input("LÜTFEN KİLONUZU GİRİNİZ:"))
 boy=float(input("LÜTFEN BOYUNUZU GİRİNİZ:"))
 formul=(kilo/boy**2)
 if(formul<=14.5):
-    print("Kilonuz Normal
-          ",formul)
+    print("Kilonuz Normal",formul)
 
 
